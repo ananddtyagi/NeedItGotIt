332c3c268970fcd4b2e3ea9cd661f3a30e5ad560
Karen Zhou <zz2202@nyu.edu>
2020-02-02 22:28:16 -0500

    adding styles for displayCard

--- needitgotit/src/components/displaycard.jsx
+++ needitgotit/src/components/displaycard.jsx
@@ -4,6 +4,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import clsx from 'clsx';
+import Collapse from '@material-ui/core/Collapse';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+
+
 
 
 
@@ -17,7 +27,41 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
-
+  title:{
+    fontSize: 14,
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  description_1: {
+    backgroundColor: '#3a3d3b',
+    color: '#fff',
+    marginTop: 5,
+    marginBottom: -10,
+    height: 80,
+    width: 300
+  },
+  description_2: {
+    backgroundColor: '#3a3d3b',
+    color: '#fff',
+    marginTop: -5,
+    width: 300,
+    marginLeft: -16
+  },
+  card: {
+    backgroundColor: '#5f6763',
+    color: '#fff'
+  },
+  button: {
+    marginTop: 5
+  }
 }));
 
 
@@ -25,15 +69,55 @@ const useStyles = makeStyles(theme => ({
   export default function DisplayCard({cardName, cardEmail, cardDescription}) {
 
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
 
     return (
       <div>
-        <Card>
+        <Card className={classes.card} variant="outlined">
+          <img 
+          alt="monster" src={`https://robohash.org/20?set=set1&size=80x100`} 
+          />
           <CardContent>
-            <Typography>{cardName}</Typography>
-            <Typography>{cardEmail}</Typography>
-            <Typography>{cardDescription}</Typography>
-
+            <Typography className={classes.title} gutterbottom>
+              Student Name: {cardName}
+            </Typography>
+            <Typography className={classes.title}>
+              Contact: {cardEmail}
+            </Typography>
+            <Typography className={classes.description_1} paragraph>
+              Project Description: {cardDescription}
+            </Typography>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography className={classes.description_2} paragraph>
+                  Detail...
+                </Typography>
+              </CardContent>
+            </Collapse>
+            <CardActions disableSpacing>
+              <IconButton className={classes.button} aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton className={classes.button} aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <IconButton className={classes.button} aria-label="person add">
+                <PersonAddIcon />
+              </IconButton>
+              <IconButton
+                className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more">
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
           </CardContent>
         </Card>
       </div>
